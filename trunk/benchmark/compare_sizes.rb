685008efa0ce93db8098d4a0b226620f42e3a27e
a.chuklin <a.chuklin@ec98882e-6de9-11de-bd2b-fffaa79d7c87>
2009-07-23 20:31:17 +0000

    Add some results concerning block size.

--- trunk/benchmark/compare_sizes.rb
+++ trunk/benchmark/compare_sizes.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-# Benchmark script for comparison of different checksum sizes 
+# Benchmark script for comparison of different rsync implementations
 # Copyright: Aleksandr Chuklin, 2009
 
 
@@ -12,21 +12,38 @@ SOURCE_DIR = BMTMP_ROOT + "/" + "source"
 DESTINATION_DIR = BMTMP_ROOT + "/" + "destination"  
 SSH_USER = 'fenix'
 SSH_SERVER = 'localhost'
+
+# "oldorig" --- original unchanged rsync binary
+# "orig" --- new binary with old options (backwards-compatible)
+# "random" --- both weak and strong sums are random
+# "randorig" --- weak sum is original while strong one is random
+
 RSYNC_TYPES_LIST = ["random"]
+
 
 # "ident" --- almost identical files (.xml)
 # "rsync" --- rsync sources (.tar.gz) [3.0.6 -> 20090507NIGHTLY, 809 KB]
 # "opera" --- opera qt3_amd64 binaries (.deb) [9.50 -> 9.51.2061, 8.9 MB]
 # "samba" --- samba sources (.tar) [3.4.0rc1 -> 3.4.0, 111.3 MB]
-# "rsyncdir" --- rsync sources (source tree) [3.0.6 -> 20090507NIGHTLY, 809 KB]
+# "rsyncdir" --- rsync sources (source tree) [3.0.6 -> 20090507NIGHTLY, 3 MB]
+# "linux" --- linux sources (.tar) [2.6.29 -> 2.6.30, 353.2 MB]
+
 TEST_PAIRS_LIST = ["opera"] 
+
+SIZES = [10, 22, 74, 100, 300, 500, 700, 800, 880, 900, 1000, 
+  2000, 2968, 3000, 4000, 5000, 10000, 20000, 38000]
+
 RSYNC_ORIG_VERSION = "3.0.6"
 RSYNC_ORIG_BIN = ROOT_DIR + "/" + "orig/rsync-#{RSYNC_ORIG_VERSION}/rsync"
 RSYNC_RANDOM_BIN = ROOT_DIR + "/" + "rsync"
-RSYNC_ORIG_OPTS_MAIN = "-avv --stats" 
-RSYNC_ORIG2_OPTS_MAIN = "-avv --stats" 
-RSYNC_RANDOM_OPTS_MAIN = "-avv --stats --random2 --random"
-RSYNC_RANDOM2_OPTS_MAIN = "-avv --stats --random2"
+RSYNC_OLDORIG_OPTS = "-avv --stats --rsync-path='#{RSYNC_ORIG_BIN}'" 
+RSYNC_ORIG_OPTS = "-avv --stats --rsync-path='#{RSYNC_RANDOM_BIN}'" 
+RSYNC_RANDOM_OPTS = "-avv --stats " \
+                    "--rsync-path='#{RSYNC_RANDOM_BIN} --random2 --random' " \
+                    "--random2 --random"
+RSYNC_RANDORIG_OPTS = "-avv --stats " \
+                    "--rsync-path='#{RSYNC_RANDOM_BIN} --random2' " \
+                    "--random2"
 #==============================================================================
 
 require 'benchmark'
@@ -43,37 +60,41 @@ TEST_PAIRS_LIST.each do |test_pair|
       original_new= ORIGINAL_DIR + "/" + test_pair + "_new"
       old = DESTINATION_DIR + "/" + test_pair + "_old"
       new = SOURCE_DIR + "/" + test_pair + "_new"
-      if rsync_type == "orig" 
-#        rsync_cmd = "#{RSYNC_ORIG_BIN} #{RSYNC_ORIG_OPTS} #{new} " +
-#                    "#{SSH_USER}@#{SSH_SERVER}:#{old}"
-        rsync_cmd = "#{RSYNC_RANDOM_BIN} #{RSYNC_ORIG2_OPTS} #{new} " +
-                    "#{SSH_USER}@#{SSH_SERVER}:#{old}"
+      rsync_targets = "#{new} #{SSH_USER}@#{SSH_SERVER}:#{old} "
+      if rsync_type == "oldorig" 
+        rsync_cmd = "#{RSYNC_ORIG_BIN} #{RSYNC_OLDORIG_OPTS} "
+      elsif rsync_type == "orig"
+        rsync_cmd = "#{RSYNC_RANDOM_BIN} #{RSYNC_ORIG_OPTS} " 
       elsif rsync_type == "random"
-        rsync_cmd = "#{RSYNC_RANDOM_BIN} #{RSYNC_RANDOM_OPTS} #{new} " +
-                    "#{SSH_USER}@#{SSH_SERVER}:#{old}"
-      elsif rsync_type == "random2"
-        rsync_cmd = "#{RSYNC_RANDOM_BIN} #{RSYNC_RANDOM2_OPTS} #{new} " +
-                    "#{SSH_USER}@#{SSH_SERVER}:#{old}"
+        rsync_cmd = "#{RSYNC_RANDOM_BIN} #{RSYNC_RANDOM_OPTS} "
+      elsif rsync_type == "randorig"
+        rsync_cmd = "#{RSYNC_RANDOM_BIN} #{RSYNC_RANDORIG_OPTS} "
       else 
         raise NotImplementedError, "Error. No such rsync type: #{rsync_type}"
       end
-      printf("\t Stats for *%s* rsync on %s test pair:\n", 
-           rsync_type.capitalize, test_pair)
-      # prepare test_pair:
-        # TODO: correct directory handling (trailing slash)
-      FileUtils.rm_rf(old)
-      FileUtils.cp_r(original_old, old)
+
+      SIZES.each do |size|
+        printf("\t Stats for *%s* rsync on %s test pair:\n", 
+             rsync_type.capitalize, test_pair)
+        puts("Block size: #{size}")
+        rsync_run_cmd = rsync_cmd + "--block-size=#{size.to_s} "
+        rsync_run_cmd += rsync_targets
+        # prepare test_pair:
+          # TODO: correct directory handling (trailing slash)
+        FileUtils.rm_rf(old)
+        FileUtils.cp_r(original_old, old)
 #      FileUtils.rm_rf(new)
 #      FileUtils.cp_r(original_new, new)
-      # now run rsync itself
-      run_time = Benchmark.realtime do
-        # puts("Starting command: #{rsync_cmd}")
-        Open3::popen3(rsync_cmd) do |stdin, stdout, stderr|
-          puts(stdout.read.strip)
-          puts(stderr.read.strip)
-        end 
+        # now run rsync itself
+        run_time = Benchmark.realtime do
+          # puts("Starting command: #{rsync_cmd}")
+          Open3::popen3(rsync_run_cmd) do |stdin, stdout, stderr|
+            puts(stdout.read.strip)
+            puts(stderr.read.strip)
+          end 
+        end
+        puts("Execution time: #{run_time}")
       end
-      puts("Execution time: #{run_time}")
     rescue NotImplementedError => message
       puts(message)
     end
@@ -81,5 +102,5 @@ TEST_PAIRS_LIST.each do |test_pair|
 end
 
 # Please, don't remove modeline
-# vim:fenc=utf-8:ft=ruby:ai:si:ts=2:sw=2:et:fdm=indent:fdn=0:
+# vim:fenc=utf-8:ft=ruby:ai:si:ts=2:sw=2:et:fdm=syntax:
 
