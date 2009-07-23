0573b7c8af5aeac9ddfa5153b9e529f7c9731cfc
a.chuklin <a.chuklin@ec98882e-6de9-11de-bd2b-fffaa79d7c87>
2009-07-23 07:07:01 +0000

    Add some TODOs (feature requests)

--- trunk/generator.c
+++ trunk/generator.c
@@ -748,7 +748,7 @@ int unchanged_file(char *fn, struct file_struct *file, STRUCT_STAT *st)
  * function to encapsulate the logic.
  *
  * The block size is a rounded square root of file length.
- *
+ * TODO: need better choice for random implementation!
  * The checksum size is determined according to:
  *     blocksum_bits = BLOCKSUM_BIAS + 2*log2(file_len) - log2(block_len)
  * provided by Donovan Baarda which gives a probability of rsync
